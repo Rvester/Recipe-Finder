8e3cbdf6308a1a821364c26531476be70289e9b5
Rvester <rasheem.khan94@gmail.com>
2025-03-14 14:07:59 -0400

    Update Vite config base path to reflect new directory structure

--- recipe-finder/vite.config.js
+++ recipe-finder/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/tree/main/recipe-finder",
+  base: process.env.VITE_BASE_PATH || "/Recipe-Finder/tree/main/recipe-finder",
 });
